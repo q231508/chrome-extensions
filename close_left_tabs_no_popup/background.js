d76bae4e29256eeef17aa133dae8c2c7da1281da
lumbani-z <lumbani@ualberta.ca>
2026-05-04 10:22:26 -0600

    added version with no popup

--- close_left_tabs_no_popup/background.js
+++ close_left_tabs_no_popup/background.js
@@ -1,21 +1,9 @@
 
-function logTabs(tabs) {
-  for (const tab of tabs) {
-    console.log(tab.id);
-  }
-}
-
-function onError(error) {
-  console.error(`Error: ${error}`);
-}
-
-document.querySelector("button").addEventListener("click", () =>{
+chrome.action.onClicked.addListener((tab) => {
   chrome.tabs.query({}, (all_tabs) => {
-    logTabs(all_tabs);
 
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
       const current_tab = tabs[0];
-      console.log(current_tab.id, "lolololol");
 
       for (let tab of all_tabs) {
         if (tab.index < current_tab.index) {
